3319c871e8682622ea9f34bf8dda25a4de83bc2a
Ashu-3110 <ashwini@thepermatech.com>
2024-05-02 15:38:20 +0530

    added a new folder

---
+++ app/backend/src/api/product-size-cost/routes/product-size-cost.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-size-cost router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-size-cost.product-size-cost');
