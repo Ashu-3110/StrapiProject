5c1c84f2d152854e88c20edfd8de498647d3efd9
Ashu-3110 <ashwini@thepermatech.com>
2024-05-02 12:12:42 +0530

    added url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url:env('PUBLIC_URL','https://striking-book-d6cfbf6b64.strapiapp.com/')
+  url:env('PUBLIC_URL','https://striking-book-d6cfbf6b64.strapiapp.com/'),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
